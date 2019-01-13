8a0431da257c84d657ba6d1d37e28d7db399fd1a
jmh <userhui@live.cn>
2019-01-13 21:06:27 +0800

    豆腐干地方的方式

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("H啊实打实ello W实打实大苏打梵蒂冈发给他发给你地方阿凡达十大官orld!");
+        System.out.println("H啊实撒旦发射点打实ello W实打实大苏打梵sdfsdfsdfsdf蒂冈发给他发给你地方阿凡达十大官orld!");
     }
 
 }
