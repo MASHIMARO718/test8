0a3e8b89ccf407d89de462c8e116363c252d9da0
jmh <userhui@live.cn>
2019-01-13 20:54:48 +0800

    豆腐干地方的方式

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello W梵蒂冈地方官orld!");
+        System.out.println("Hello W梵蒂冈发给他发给你地方官orld!");
     }
 
 }
